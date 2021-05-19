91ee1b78c35591b0ef127deb123836c7710fd7ed
lianlian51 <3282125330@qq.com>
2021-05-19 23:28:21 +0800

    AVL树

--- container/BSTree/BSTree/testBST.cpp
+++ container/BSTree/BSTree/testBST.cpp
@@ -1,5 +1,8 @@
-#include "BSTree.hpp"
-
+// #include "BSTree.hpp"
+//#include "AVLTree.hpp"
+//#include <iostream>
+//using namespace std;
+#if 0
 int main()
 {
 	int arr[] = { 5, 3, 4, 1, 7, 8, 2, 6, 0, 9 };
@@ -27,5 +30,63 @@ int main()
 		std::cout << "5 删除 ！" << std::endl;
 	}
 
+	return 0;
+}
+#endif
+
+#if 0
+int main()
+{
+	int arr1[] = { 16, 3, 7, 11, 9, 26, 18, 14, 15 };
+	int arr2[] = { 4, 2, 6, 1, 3, 5, 15, 7, 16, 14 };
+	AVLTree<int> avl1, avl2;
+	for (auto& e : arr1)
+		avl1.Insert(e);
+	for (auto& e : arr2)
+		avl2.Insert(e);
+
+	bool isBalance = avl1.isBalanceTree();
+	if (isBalance)
+	{
+		std::cout << "avl1 是平衡的！！！" << std::endl;
+	}
+	else
+	{
+		std::cout << "avl1 是不平衡的！！！" << std::endl;
+	}
+	avl1.InOrder();
+	std::cout << std::endl;
+
+	isBalance = avl2.isBalanceTree();
+	if (isBalance)
+	{
+		std::cout << "avl2 是平衡的！！！" << std::endl;
+	}
+	else
+	{
+		std::cout << "avl2 是不平衡的！！！" << std::endl;
+	}
+	avl2.InOrder();
+	std::cout << std::endl;
+
+
+	return 0;
+}
+
+#endif
+
+
+// #include "RBTree.hpp"
+// #include "map.h"
+// #include "set.h"
+
+#include "hashBucket.hpp"
+int main()
+{
+	// TestAVLTree();
+	// TestRBTree();
+	// TestMap();
+	// TestSet();
+	TestHashBucket();
 	return 0;
 }
